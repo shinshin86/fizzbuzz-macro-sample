233665db6ad3f0be0af8c726dc59063e4ef55b2e
shinshin86 <beagles1986@gmail.com>
2023-01-22 08:28:06 +0900

    First commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+macro_rules! fizzbuzz {
+    ($n:expr) => {
+        if ($n % 15 == 0) {
+            println!("fizzbuzz");
+        } else if ($n % 5 == 0) {
+            println!("buzz");
+        } else if ($n % 3 == 0) {
+            println!("fizz");
+        } else {
+            println!("{}", $n);
+        }
+    };
+}
+
+fn main() {
+    for n in 1..101 {
+        fizzbuzz!(n);
+    }
+}
